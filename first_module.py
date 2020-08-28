65b742746c21d0069fa233d957d700ed06f75f5c
Robin Yadav <48661780+robinyUArizona@users.noreply.github.com>
2020-08-28 02:59:50 -0500

    Add files via upload

---
+++ first_module.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 28 02:00:58 2020
+
+@author: yrobi
+"""
+
+def say_hello():
+    print("Hello!")
+
+
+def check_add(a, b):
+    x = a + b
+    return x
+
+def check_sum(a, b):
+    x = a - b
+    return x
